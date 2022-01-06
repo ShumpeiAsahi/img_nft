bb82d59499ae190c3b806c7d9d10641db90f27e0
Shumpei Asahi <a.s.mail071275@gmail.com>
2022-01-06 20:36:11 +0900

    210106

--- solidity/factoryNFT.sol
+++ solidity/factoryNFT.sol
@@ -1,18 +1,17 @@
 pragma solidity 0.8.6;
 
-import "https://github.com/0xcert/ethereum-erc721/src/contracts/tokens/nf-token-metadata.sol";
-import "https://github.com/0xcert/ethereum-erc721/src/contracts/ownership/ownable.sol";
+import "https://github.com/nibbstack/erc721/tree/master/src/contracts/tokens/nf-token-metadata.sol";
+import "https://github.com/nibbstack/erc721/tree/master/src/contracts/ownership/ownable.sol";
 
 
 contract NewNFT is NFTokenMetadata, Ownable {
-    using Counters for Counters.Counter;
-    Counters.Counter private _tokenIds;
 
     constructor(){
         nftName = "Synth NFT";
         nftSymbol = "SYN";
      }
     
+    //_uriにIPFSのアドレスが入る
     function mint(address _to ,uint256 _tokenId, string calldata _uri) external onlyOwner{
         super._mint(_to, _tokenId);
         super._setTokenUri(_tokenId, _uri);
