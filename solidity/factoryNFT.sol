dfcaee2b41537c9436fa4b4bb0fd50bed2ed723f
ASAHISURFACE\asmai <a.s.mail071275@gmail.com>
2022-01-08 17:53:38 +0900

    url変更

--- solidity/factoryNFT.sol
+++ solidity/factoryNFT.sol
@@ -1,7 +1,7 @@
 pragma solidity 0.8.6;
 
-import "https://github.com/nibbstack/erc721/tree/master/src/contracts/tokens/nf-token-metadata.sol";
-import "https://github.com/nibbstack/erc721/tree/master/src/contracts/ownership/ownable.sol";
+import "https://github.com/nibbstack/erc721/blob/master/src/contracts/tokens/nf-token-metadata.sol";
+import "https://github.com/nibbstack/erc721/blob/master/src/contracts/ownership/ownable.sol";
 
 contract NewNFT is NFTokenMetadata, Ownable {
 
