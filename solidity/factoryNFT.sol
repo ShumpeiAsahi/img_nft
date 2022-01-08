cc9306f2dfa7577e1275e403ea38d7331e3908c0
ASAHISURFACE\asmai <a.s.mail071275@gmail.com>
2022-01-08 15:37:49 +0900

    いったんコミット

--- solidity/factoryNFT.sol
+++ solidity/factoryNFT.sol
@@ -3,7 +3,6 @@ pragma solidity 0.8.6;
 import "https://github.com/nibbstack/erc721/tree/master/src/contracts/tokens/nf-token-metadata.sol";
 import "https://github.com/nibbstack/erc721/tree/master/src/contracts/ownership/ownable.sol";
 
-
 contract NewNFT is NFTokenMetadata, Ownable {
 
     constructor(){
